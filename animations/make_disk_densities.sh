b9ce12319821c7df57bb956f0ae20ad0a1a8a672
colin <colin.leach@comcast.net>
2020-05-24 10:54:28 -0700

    animations, report summary

--- animations/make_disk_densities.sh
+++ animations/make_disk_densities.sh
@@ -1,3 +1,7 @@
-ffmpeg -r 10  -start_number 0  -s 1920x1080 -i png_files/MW_density_early_%03d.png -vcodec libx264 -vf fps=25 -crf 25  -pix_fmt yuv420p MW_disk_60.mp4
-ffmpeg -r 10  -start_number 0  -s 1920x1080 -i png_files/M31_density_early_%03d.png -vcodec libx264 -vf fps=25 -crf 25  -pix_fmt yuv420p M31_disk_60.mp4
-ffmpeg -r 10  -start_number 0  -s 1920x1080 -i png_files/M33_density_early_%03d.png -vcodec libx264 -vf fps=25 -crf 25  -pix_fmt yuv420p M33_disk_60.mp4
+#ffmpeg -r 10  -start_number 0  -s 1920x1080 -i png_files/MW_density_early_%03d.png -vcodec libx264 -vf fps=25 -crf 25  -pix_fmt yuv420p MW_disk_60.mp4
+#ffmpeg -r 10  -start_number 0  -s 1920x1080 -i png_files/M31_density_early_%03d.png -vcodec libx264 -vf fps=25 -crf 25  -pix_fmt yuv420p M31_disk_60.mp4
+#ffmpeg -r 10  -start_number 0  -s 1920x1080 -i png_files/M33_density_early_%03d.png -vcodec libx264 -vf fps=25 -crf 25  -pix_fmt yuv420p M33_disk_60.mp4
+
+ffmpeg -r 10  -start_number 0  -s 1920x1080 -i png_files/MW_disk_30_%03d.png -vcodec libx264 -vf fps=25 -crf 25  -pix_fmt yuv420p MW_disk_30.mp4
+ffmpeg -r 10  -start_number 0  -s 1920x1080 -i png_files/M31_disk_30_%03d.png -vcodec libx264 -vf fps=25 -crf 25  -pix_fmt yuv420p M31_disk_30.mp4
+ffmpeg -r 10  -start_number 0  -s 1920x1080 -i png_files/M33_disk_15_%03d.png -vcodec libx264 -vf fps=25 -crf 25  -pix_fmt yuv420p M33_disk_15.mp4
